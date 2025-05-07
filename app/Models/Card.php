eefac45ec17f36678dd3f564475aff63fcbe2354
your username <your email address>
2025-05-07 18:42:58 +0300

    done79

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -49,7 +49,7 @@ class Card extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class); 
     }
 
     /**
