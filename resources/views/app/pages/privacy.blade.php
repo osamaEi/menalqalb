8bf2201b842d46e8804aac8fb0b7fbd1a3493825
your username <your email address>
2025-05-24 17:35:54 +0300

    done

--- resources/views/app/pages/privacy.blade.php
+++ resources/views/app/pages/privacy.blade.php
@@ -4,11 +4,9 @@
 <div class="app white messagebox">
   
     <div class="row justify-content-center">
-        <div class="container mx-auto px-4 py-8">
-            <h1 class="text-3xl font-bold mb-4">{{ $title }}</h1>
-            <div class="prose max-w-none">
-                {!! $description !!}
-            </div>
+        <div class="col-12 col-lg-4">
+            <h3 class="text-center text-[24px] font-[900] text-[#242424] mb-3">إتمام القفل</h3>
+            <p class="text-center">هنا يمكنك إتمام إعداد القفل أو عرض التفاصيل.</p>
         </div>
     </div>
 </div>
